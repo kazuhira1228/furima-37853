5aae64a14a6c0b7d6586ac7da22d7064bf5a8b85
kazzhrsw <kazuhira_hrsw@icloud.com>
2022-08-28 16:34:32 +0900

    商品詳細表示機能　1st

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: :index
-  # before_action :move_to_index, except: :index  LGTMがもらえたら、削除する
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,16 +18,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :postage_id, :region_id, :shopping_date_id, :price, :image).merge(user_id: current_user.id)
   end
-
-  # def move_to_index  LGTMがもらえたら、削除する
-  #   unless user_signed_in?
-  #     redirect_to new_user_session_path
-  #   end
-  # end
 
 end
