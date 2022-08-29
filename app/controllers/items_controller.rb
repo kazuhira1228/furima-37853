8540c7238bcae35a4d863db35a7eb22c13fde2a1
kazzhrsw <kazuhira_hrsw@icloud.com>
2022-08-29 17:32:01 +0900

    修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :url_direct_hit_prevention, only: :edit     # 学習用メモ：def edit 〜 end より先に、このコードが読み込まれる。
 
   def index
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.update(item_params)
       redirect_to action: :show
@@ -47,8 +45,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :postage_id, :region_id, :shopping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def url_direct_hit_prevention
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def url_direct_hit_prevention
     if @item.user.id != current_user.id
       redirect_to root_path
     end
