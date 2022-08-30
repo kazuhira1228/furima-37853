b05e926b6469b1606ace4e8a7185d01692a6412c
kazzhrsw <kazuhira_hrsw@icloud.com>
2022-08-30 16:02:14 +0900

    修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :url_direct_hit_prevention, only: :edit     # 学習用メモ：def edit 〜 end より先に、このコードが読み込まれる。
+  before_action :url_direct_hit_prevention, only: [:edit, :destroy]     # 学習用メモ：def edit 〜 end より先に、このコードが読み込まれる。
 
   def index
     @items = Item.order("created_at DESC")
